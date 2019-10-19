d89e5bb53b876846377a24951a51ec3ce3b44002
Chris Peterson <Chris.peterson444@gmail.com>
2019-10-19 02:21:18 -0700

    First half of K_Means_better complete need to finish and test whole file.

--- clustering.py
+++ clustering.py
@@ -2,7 +2,7 @@
 # @Author: Chris Peterson
 # @Date:   2019-10-17 16:52:38
 # @Last Modified by:   Chris Peterson
-# @Last Modified time: 2019-10-19 00:38:34
+# @Last Modified time: 2019-10-19 02:20:25
 import numpy as np
 import random as rand
 import copy
@@ -74,7 +74,48 @@ def K_Means(X,K):
 					
 			clusters_previous = copy.deepcopy(clusters)
 			clusters = [[] for i in range(0,K)]
-
 	return cluster_centers
 		
-			
+
+#
+# K_Means_better(X,K)
+# @param X A numpy array of features, indexed by sample
+# @param K An integer representing the number of cluster centers desired
+# @return cluster_centers A numpy array of (float) cluster centers that was found to be the best over many iterations
+# 						of k_means(X,K)
+#			
+def K_Means_better(X,K):
+	models = []
+	model_votes = []
+	cluster_centers = []
+	MIN_ITERATIONS = 1000
+	MAX_ITERATIONS = 10000
+	iteration_number = 0
+	found_majority = False
+	sigma = .05
+
+
+	while iteration_number < MIN_ITERATIONS:
+		current_model = K_Means(X,K)
+		closest_match = [[], float("inf")]
+
+
+		if current_model in models:
+			index = models.index(current_model)
+			model_votes[index] = model_votes[index] + 1
+		else:
+			for model in models:
+				difference = np.linalg.norm(np.subtract(model,current_model))
+				if difference < closest_match[1]:
+					closest_match = [model, difference]
+			if closest_match[1] < sigma:
+				index = models.index(closest_match[0])
+				model_votes[index] = model_votes[index] + 1
+
+			else:
+				models.append(current_model)
+				model_votes.append(1)
+
+
+		iteration_number = iteration_number + 1
+		
