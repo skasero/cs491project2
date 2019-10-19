557187ea8d21d85bcf2a0bb1385cbe2e159eea10
Chris Peterson <Chris.peterson444@gmail.com>
2019-10-19 13:19:04 -0700

    Fixed k_means. Next is k_means_better

--- clustering.py
+++ clustering.py
@@ -2,7 +2,7 @@
 # @Author: Chris Peterson
 # @Date:   2019-10-17 16:52:38
 # @Last Modified by:   Chris Peterson
-# @Last Modified time: 2019-10-19 11:47:26
+# @Last Modified time: 2019-10-19 13:17:28
 import numpy as np
 import random as rand
 import copy
@@ -18,20 +18,22 @@ def K_Means(X,K):
 	# set up an array for cluster centers (grabs a random sample's features)
 	cluster_centers = []
 
+
 	# Seed the random number generator for cluster center initialization
 	rand.seed()
 
 	# Randomly choose K samples as initial cluster centers
 	while len(cluster_centers) < K:
 		i = rand.randint(0,len(X) - 1)   # Note: len(X) - 1 Since randint(a,b) generates [a,b]
-		if X[i] not in cluster_centers:
-			cluster_centers.append(X[i])
-
+		center = X[i].tolist()
+		if center not in cluster_centers:
+			cluster_centers.append(center)
+	# print(cluster_centers)
 	#Flag to determine if main algorithm has converged
 	converged = False
 
 	# Array to store cluster from previous run to determine if convergence has happened.
-	clusters_previous = np.array([])
+	clusters_previous = []
 
 	# clusters is a list of the samples (stored by index of X) that belong to a particluar cluster
 	# The indicies for clusters is meant to be the same for cluster_centers
@@ -49,10 +51,11 @@ def K_Means(X,K):
 		for i in range(0, len(X)):
 			best_info = [-1,float("inf")]
 			for j in range(0,len(cluster_centers)):
-				distance = np.linalg.norm(cluster_centers[j] - X[i])
+				distance = np.linalg.norm(np.subtract(cluster_centers[j],X[i])).tolist()
+				# print(distance)
 				if distance < best_info[1]:
 					best_info = [j,distance]
-			clusters[best_info[0]].append(i)
+			clusters[best_info[0]].append(X[i].tolist())
 
 		# Check if anything has changed from last iteration
 		if np.array_equal(clusters, clusters_previous):
@@ -61,13 +64,15 @@ def K_Means(X,K):
 			# Recompute cluster centers
 			for i in range(0, len(cluster_centers)):
 				if not np.array_equal(clusters[i], []):
-					cluster_centers[i] = [np.mean(clusters[i], axis=0, dtype=float)]
+					print("Cluster i: ", clusters[i])
+					print("Mean: ", np.mean(clusters[i], axis=0, dtype=float))
+					cluster_centers[i] = np.mean(clusters[i], axis=0, dtype=float)
 				# else: Points could be reassigned to the empty cluster later. Lets keep the center
 					
 					
 			clusters_previous = copy.deepcopy(clusters)
 			clusters = [[] for i in range(0,K)]
-	return cluster_centers
+	return np.array(cluster_centers)
 		
 
 #
@@ -91,6 +96,8 @@ def K_Means_better(X,K):
 
 	while iteration_number < MAX_ITERATIONS and not found_majority:
 		current_model = K_Means(X,K)
+		current_model = current_model.tolist()
+
 		closest_match = [[], float("inf")]
 
 
